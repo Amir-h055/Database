eb9a949c06d8414b1ecd3fb1286b7fc4f3d9aab0
Nathan AV <nathan.alix88@gmail.com>
2021-07-18 12:21:15 -0400

    Added create statements

--- create_relations.sql
+++ create_relations.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Person(
+CREATE TABLE Person (
   medicaidNum  VARCHAR(20),
   telephone VARCHAR(13),
   firstName VARCHAR(100),
@@ -10,4 +10,53 @@ CREATE TABLE Person(
   citizenship BOOLEAN,
   email VARCHAR(255),
   PRIMARY KEY(medicaidNum)
+);
+
+CREATE TABLE Infection (
+  dateInfection DATE,
+  medicaidNum VARCHAR(20),
+  PRIMARY KEY(dateInfection, medicaidNum)
+);
+
+CREATE TABLE AgeGroup (
+  ageRange VARCHAR(20),
+  isCurrent BOOLEAN,
+  PRIMARY KEY (ageRange)
+);
+
+CREATE TABLE PersonAgeGroup (
+  ageRange VARCHAR(20),
+  medicaidNum VARCHAR(20),
+  PRIMARY KEY(ageRange, medicaidNum)
+);  
+
+CREATE TABLE VaccinationDrug (
+  name VARCHAR(100),
+  status VARCHAR(50),
+  dateLastStatus DATE,
+  PRIMARY KEY (name)
+);
+
+CREATE TABLE VaccinationDoneWith (
+  medicaidNum VARCHAR(20),
+  doseNumber INT,
+  name VARCHAR(100),
+  PRIMARY KEY (medicaidNum, doseNumber, name)
+);
+
+CREATE TABLE VaccinationDoneAt (
+  medicaidNum VARCHAR(20),
+  doseNumber INT,
+  name VARCHAR(100),
+  address VARCHAR(100),
+  PRIMARY KEY(medicaidNum, doseNumber, name, address)
+);
+
+CREATE TABLE HealthFacility (
+  name VARCHAR(100),
+  address VARCHAR(100),
+  phoneNumber VARCHAR(13),
+  webAddress VARCHAR(100),
+  type VARCHAR(8),
+  PRIMARY KEY (name, address)
 );
