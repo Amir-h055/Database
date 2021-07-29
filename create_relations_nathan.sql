cc450d57a90f56f10c666bf69e7e4eca4b01c4f1
Nathan AV <nathan.alix88@gmail.com>
2021-07-29 19:43:58 -0400

    Added script to create relations SQL

---
+++ create_relations_nathan.sql
@@ -0,0 +1,195 @@
+CREATE DATABASE PROJECT;
+
+USE PROJECT;
+
+CREATE TABLE PROJECT.Person (
+  passportNumOrSSN VARCHAR(10),
+  medicaidNum  VARCHAR(10),
+  telephone VARCHAR(13),
+  firstName VARCHAR(100),
+  lastName VARCHAR(100),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(3),
+  citizenship BOOLEAN,
+  email VARCHAR(255),
+  dateOfBirth DATE,
+  PRIMARY KEY(passportNumOrSSN)
+);
+
+CREATE TABLE PROJECT.Infection (
+  dateInfection DATE,
+  passportNumOrSSN VARCHAR(10),
+  type VARCHAR(100),
+  PRIMARY KEY(passportNumOrSSN, dateInfection),
+  FOREIGN KEY (passportNumOrSSN) REFERENCES Person(passportNumOrSSN)
+);
+
+CREATE TABLE PROJECT.AgeGroup (
+  ageRange VARCHAR(20),
+  PRIMARY KEY (ageRange)
+);
+
+CREATE TABLE PROJECT.Vaccination (
+  passportNumOrSSN VARCHAR(10),
+  doseNumber INT,
+  date DATE,
+  PRIMARY KEY (passportNumOrSSN, doseNumber),
+  FOREIGN KEY (passportNumOrSSN) REFERENCES Person(passportNumOrSSN)
+);
+
+CREATE TABLE PROJECT.PersonAgeGroup (
+  ageRange VARCHAR(20),
+  passportNumOrSSN VARCHAR(10),
+  PRIMARY KEY (ageRange, passportNumOrSSN),
+  FOREIGN KEY (passportNumOrSSN) REFERENCES Person(passportNumOrSSN),
+  FOREIGN KEY (ageRange) REFERENCES AgeGroup(ageRange)
+);  
+
+CREATE TABLE PROJECT.VaccinationDrug (
+  name VARCHAR(100),
+  PRIMARY KEY (name)
+);
+
+CREATE TABLE PROJECT.HealthFacility (
+  name VARCHAR(100),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(3),
+  phoneNumber VARCHAR(13),
+  webAddress VARCHAR(100),
+  telephone VARCHAR(13),
+  type VARCHAR(8),
+  PRIMARY KEY (name, address)
+);
+
+CREATE TABLE PROJECT.VaccinationDoneWith (
+  passportNumOrSSN VARCHAR(10),
+  doseNumber INT,
+  name VARCHAR(100),
+  PRIMARY KEY (passportNumOrSSN, doseNumber, name),
+  FOREIGN KEY (passportNumOrSSN) REFERENCES Person(passportNumOrSSN),
+  FOREIGN KEY (name) REFERENCES VaccinationDrug(name),
+  FOREIGN KEY (doseNumber) REFERENCES Vaccination(doseNumber)
+);
+
+CREATE TABLE PROJECT.VaccinationDoneAt (
+  passportNumOrSSN VARCHAR(10),
+  doseNumber INT,
+  name VARCHAR(100),
+  address VARCHAR(100),
+  PRIMARY KEY (passportNumOrSSN, doseNumber, name, address),
+  FOREIGN KEY (passportNumOrSSN) REFERENCES Person(passportNumOrSSN),
+  FOREIGN KEY (doseNumber) REFERENCES Vaccination(doseNumber),
+  FOREIGN KEY (name,address) REFERENCES HealthFacility(name,address)
+);
+
+CREATE TABLE PROJECT.CurrentAgeGroup {
+  ageRange VARCHAR(20),
+  province VARCHAR(3),
+  PRIMARY KEY (ageRange),
+  FOREIGN KEY (ageRange) REFERENCES AgeGroup(ageRange)
+}
+
+CREATE TABLE PROJECT.DrugHistory {
+  name VARCHAR(100),
+  date DATE,
+  status VARCHAR(100),
+  PRIMARY KEY (name, date),
+  FOREIGN KEY (name) REFERENCES VaccinationDrug(name)
+}
+
+CREATE TABLE PROJECT.VaccinationDoneBy {
+  passportNumOrSSN VARCHAR(10),
+  doseNumber INT,
+  EID VARCHAR(10),
+  PRIMARY KEY (passportNumOrSSN, doseNumber),
+  FOREIGN KEY (passportNumOrSSN) Person(passportNumOrSSN),
+  FOREIGN KEY (doseNumber) Vaccination(doseNumber),
+  FOREIGN KEY (EID) Employee(EID)
+}
+
+CREATE TABLE PROJECT.Employee {
+  EID VARCHAR(10),
+  SSN VARCHAR(10),
+  firstName VARCHAR(100),
+  lastName VARCHAR(100), 
+  medicare VARCHAR(100),
+  dateOfBirth DATE,
+  telephone VARCHAR(13),
+  address VARCHAR(100), 
+  city VARCHAR(100),
+  province VARCHAR(3),
+  citizenship BOOLEAN, 
+  email VARCHAR(100),
+  PRIMARY KEY (EID)
+}
+
+CREATE TABLE PROJECT.Manager {
+  EID VARCHAR(10),
+  PRIMARY KEY (EID),
+  FOREIGN KEY (EID) Employee(EID)
+}
+
+CREATE TABLE PROJECT.Manages {
+  EID VARCHAR(10),
+  name VARCHAR(100),
+  address VARCHAR(100),
+  PRIMARY KEY (EID),
+  FOREIGN KEY (EID) REFERENCES Manager(EID),
+  FOREIGN KEY (name, address) REFERENCES HealthFacility(name, address)
+}
+
+CREATE TABLE Project.JobHistory {
+  EID VARCHAR(10), 
+  name VARCHAR(100),
+  address VARCHAR(100), 
+  startDate DATE,
+  endDate DATE,
+  PRIMARY KEY (EID, name, address, startDate),
+  FOREIGN KEY (EID) REFERENCES Employee(EID),
+  FOREIGN KEY (name, address) REFERENCES HealthFacility(name, address)
+}
+
+CREATE TABLE Project.VaccineStored {
+  nameHSO VARCHAR(100), 
+  address VARCHAR(100), 
+  nameDrug VARCHAR(100), 
+  count INT,
+  PRIMARY KEY (nameHSO, address, nameDrug),
+  FOREIGN KEY (nameHSO, address) REFERENCES HealthFacility(name, address),
+  FOREIGN KEY (nameDrug) REFERENCES Vaccination(name)
+}
+
+CREATE TABLE VaccineShipment {
+  nameHSO VARCHAR(100), 
+  address VARCHAR(100), 
+  nameDrug VARCHAR(100),
+  date DATE,
+  count INT,
+  PRIMARY KEY (nameHSO, address, nameDrug, date),
+  FOREIGN KEY (nameHSO, address) REFERENCES HealthFacility(name, address),
+  FOREIGN KEY (nameDrug) REFERENCES Vaccination(name)
+}
+
+CREATE TABLE VaccineTransfer {
+  nameHSOFrom VARCHAR(100), 
+  nameHSOTo VARCHAR(100), 
+  addressFrom VARCHAR(100), 
+  addressTo VARCHAR(100), 
+  nameDrug VARCHAR(100),
+  date DATE,
+  count INT,
+  PRIMARY KEY (nameHSOFrom,nameHSOTo, addressFrom, addressTo, nameDrug, date),
+  FOREIGN KEY (nameHSOFrom, addressFrom) REFERENCES HealthFacility(name, address),
+  FOREIGN KEY (nameHSOTo, addressTo) REFERENCES HealthFacility(name, address),
+  FOREIGN KEY (nameDrug) REFERENCES Vaccination(name)
+}
+
+CREATE TABLE PostalCode {
+  address VARCHAR(100), 
+  city VARCHAR(100),
+  province VARCHAR(3),
+  postalCode VARCHAR(6),
+  PRIMARY KEY (address, city, province)
+}
