e22e32729fac200b403904a1f102f833f2477da1
Amir-h055 <78324151+Amir-h055@users.noreply.github.com>
2021-08-08 16:53:30 +0430

    change provinceID back to int

--- populate_relations.sql
+++ populate_relations.sql
@@ -12,49 +12,49 @@ INSERT AgeGroup VALUES (8, "12-17");
 INSERT AgeGroup VALUES (9, "5-11");
 INSERT AgeGroup VALUES (10, "0-4");
 
-INSERT ProvinceCurrentAgeGroup VALUES ("NL", 4);
-INSERT ProvinceCurrentAgeGroup VALUES ("PE", 5);
-INSERT ProvinceCurrentAgeGroup VALUES ("NS", 6);
-INSERT ProvinceCurrentAgeGroup VALUES ("NB", 3);
-INSERT ProvinceCurrentAgeGroup VALUES ("QC", 8);
-INSERT ProvinceCurrentAgeGroup VALUES ("ON", 1);
-INSERT ProvinceCurrentAgeGroup VALUES ("MB", 2);
-INSERT ProvinceCurrentAgeGroup VALUES ("SK", 9);
-INSERT ProvinceCurrentAgeGroup VALUES ("AB", 4);
-INSERT ProvinceCurrentAgeGroup VALUES ("BC", 6);
-INSERT ProvinceCurrentAgeGroup VALUES ("YT", 7);
-INSERT ProvinceCurrentAgeGroup VALUES ("NT", 8);
-INSERT ProvinceCurrentAgeGroup VALUES ("NU", 4);
+INSERT ProvinceCurrentAgeGroup VALUES (1, "NL", 4);
+INSERT ProvinceCurrentAgeGroup VALUES (2, "PE", 5);
+INSERT ProvinceCurrentAgeGroup VALUES (3, "NS", 6);
+INSERT ProvinceCurrentAgeGroup VALUES (4, "NB", 3);
+INSERT ProvinceCurrentAgeGroup VALUES (5, "QC", 8);
+INSERT ProvinceCurrentAgeGroup VALUES (6, "ON", 1);
+INSERT ProvinceCurrentAgeGroup VALUES (7, "MB", 2);
+INSERT ProvinceCurrentAgeGroup VALUES (7, "SK", 9);
+INSERT ProvinceCurrentAgeGroup VALUES (9, "AB", 4);
+INSERT ProvinceCurrentAgeGroup VALUES (10, "BC", 6);
+INSERT ProvinceCurrentAgeGroup VALUES (11, "YT", 7);
+INSERT ProvinceCurrentAgeGroup VALUES (12, "NT", 8);
+INSERT ProvinceCurrentAgeGroup VALUES (13, "NU", 4);
 
 INSERT Person VALUES ("5418600012", "1936638 14", "(514)482-4299", "Annabel", "Dodson",
-  "6860 Fielding", "Montreal", 7, "QC", TRUE,
+  "6860 Fielding", "Montreal", 7, 5, TRUE,
   "annabel.dodson@gmail.com", "1996-08-06");
 INSERT Person VALUES ("7198638080", "1362899 55","(514)366-4286", "Zachary", "Rutledge",
-  "902 Tittley", "Montreal", 6, "QC", TRUE,
+  "902 Tittley", "Montreal", 6, 5, TRUE,
   "Zachary.Rutledge@gmail.com", "1986-08-12");
 INSERT Person VALUES ("5867167004", "0243401 87", "(514)767-5030", "Alister", "Wiggins",
-  "6818 Lamont", "Montreal", 6, "QC", TRUE, "Alister.Wiggins@gmail.com",
+  "6818 Lamont", "Montreal", 6, 5, TRUE, "Alister.Wiggins@gmail.com",
   "1987-08-27"); 
 INSERT Person VALUES ("9415548075", "2054252 15", "(514)525-4731", "Osman", "Vaughn", 
-  "1720 Bourbonniere", "Montreal", 8, "QC", TRUE,
+  "1720 Bourbonniere", "Montreal", 8, 5, TRUE,
   "Osman.Vaughn@gmail.com", "2007-09-08"); 
 INSERT Person VALUES ("9052864070", "1643994 57", "(514)768-9102", "Bear", "Melton",
-  "5962 Jogues", "Montreal", 5, "QC", TRUE, "Bear.Melton@gmail.com",
+  "5962 Jogues", "Montreal", 5, 5, TRUE, "Bear.Melton@gmail.com",
   "1976-09-13"); 
 INSERT Person VALUES ("9826293018", "4105748 51", "(613)733-8502", "Lulu", "Fisher",
-  "927 Rand", "Gatineau", 5, "QC", TRUE, "Lulu.Fisher@gmail.com",
+  "927 Rand", "Gatineau", 5, 5, TRUE, "Lulu.Fisher@gmail.com",
   "1976-09-27"); 
 INSERT Person VALUES ("2055054040", "4990628 77", "(819)503-3196", "Collette", "Zavala",
-  "60 Du Blizzard", "Gatineau", 3, "QC", TRUE,
+  "60 Du Blizzard", "Gatineau", 3, 5, TRUE,
   "Collette.Zavala@gmail.com", "1956-09-29"); 
 INSERT Person VALUES ("7247613020", "3234394 08", "(819)408-0531", "Angela", "Dodson",
-  "1175 De L'Esplanade", "Sherbrooke", 3, "QC", TRUE,
+  "1175 De L'Esplanade", "Sherbrooke", 3, 5, TRUE,
   "Angela.Dodson@gmail.com", "1956-10-15"); 
 INSERT Person VALUES ("7976980046", "9109123 89", "(819)566-0668", "Annabel", "Crouch",
-  "1812 Dunant", "Sherbrooke", 1, "QC", TRUE,
+  "1812 Dunant", "Sherbrooke", 1, 5, TRUE,
   "Annabel.Crouch@gmail.com", "1936-10-18"); 
 INSERT Person VALUES ("6623218089", "0451174 26", "(418)547-8256", "Nyle", "Sparrow",
-  "3937 Soucy", "Jonquière", 1, "QC", TRUE, "Nyle.Sparrow@gmail.com",
+  "3937 Soucy", "Jonquière", 1, 5, TRUE, "Nyle.Sparrow@gmail.com",
   "1936-12-08"); 
 
 INSERT VariantType VALUES(1, "ALPHA");
@@ -86,29 +86,29 @@ INSERT VaccinationDrug VALUES ("KoviVac");
 INSERT VaccinationDrug VALUES ("EpiVacCorona");
 
 INSERT HealthFacility VALUES ("Olympic Stadium",
-  "4545 Avenue Pierre-De Coubertin", "Montreal", "QC", "(514)252-4141",
+  "4545 Avenue Pierre-De Coubertin", "Montreal", 5, "(514)252-4141",
   "www.so.com", "SPECIAL"); 
 INSERT HealthFacility VALUES ("Jewish General Hospital",
-  "3755 Chemin de la Côte-Sainte-Catherine", "Montreal", "QC", "www.gjw.com",
+  "3755 Chemin de la Côte-Sainte-Catherine", "Montreal", 5, "www.gjw.com",
   "(514)340-8222", "HOSPITAL"); 
 INSERT HealthFacility VALUES ("Hopital de Gatineau",
-  "909 Boulevard la Vérendrye O", "Gatineau", "QC", "(819)966-6100",
+  "909 Boulevard la Vérendrye O", "Gatineau", 5, "(819)966-6100",
   "www.hg.com", "HOSPITAL"); 
-INSERT HealthFacility VALUES ("CHUS", "300 Rue King E", "Sherbrooke", "QC", 
+INSERT HealthFacility VALUES ("CHUS", "300 Rue King E", "Sherbrooke", 5, 
   "www.chus.com", "(819)346-1110", "HOSPITAL");
 INSERT HealthFacility VALUES ("Hôpital Fleury", "2180, rue Fleury Est",
-  "Montreal", "QC", "(514)384-2000", "www.hopitalFleury.com","HOSPITAL");
+  "Montreal", 5, "(514)384-2000", "www.hopitalFleury.com","HOSPITAL");
 INSERT HealthFacility VALUES ("Hôpital Richardson", "5425, Avenue Bessborough",
-  "Montreal", "QC","(514)484-7878", "www.hopitalRichardson.com","HOSPITAL");
+  "Montreal", 5,"(514)484-7878", "www.hopitalRichardson.com","HOSPITAL");
 INSERT HealthFacility VALUES ("Hôpital Rivière-des-Prairies",
-  "7070, boulevard Perras", "Montreal", "QC", "(514)323-7260",
+  "7070, boulevard Perras", "Montreal", 5, "(514)323-7260",
   "www.hopitalRP.com","HOSPITAL");
 INSERT HealthFacility VALUES ("Hôpital de Lasalle", "8585, Terrasse Champlain",
-  "LaSalle", "QC", "(514)362-8000", "www.hopitalLasalle.com","HOSPITAL");
+  "LaSalle", 5, "(514)362-8000", "www.hopitalLasalle.com","HOSPITAL");
 INSERT HealthFacility VALUES ("Hôpital de Verdun", "4000, boul. Lasalle",
-  "Verdun", "QC", "(514)362-1100", "www.hopitalVerdun.com","HOSPITAL");
+  "Verdun", 5, "(514)362-1100", "www.hopitalVerdun.com","HOSPITAL");
 INSERT HealthFacility VALUES ("Hôpital de Sainte-Anne",
-  "305, boulevard des Anciens-Combattants", "Sainte-Anne-de-Bellevue", "QC",
+  "305, boulevard des Anciens-Combattants", "Sainte-Anne-de-Bellevue", 5,
   "(514)457-3440", "www.hopitalSaintAnne.com","HOSPITAL");
 
 INSERT DrugHistory VALUES ("Pfizer", "2020-06-21", "SAFE");
@@ -124,33 +124,33 @@ INSERT DrugHistory VALUES ("EpiVacCorona", "2021-01-01", "SUSPENDED");
 
 INSERT Employee VALUES ("2314904771","4030141599", "6202715 46", "Elizabeth",
   "Jernigan", "1996-04-18", "(418)640-9486", "4884 Boulevard Cremazie",
-  "Quebec", "QC", TRUE, "eliJer@gmail.com");
+  "Quebec", 5, TRUE, "eliJer@gmail.com");
 INSERT Employee VALUES ("4091939153","3559893762", "8331149 83", "William",
   "Blackman", "1991-07-01", "(418)299-4800", "3619 avenue de Port-Royal",
-  "Bonaventure", "QC", TRUE, "wilBlack@gmail.com");
+  "Bonaventure", 5, TRUE, "wilBlack@gmail.com");
 INSERT Employee VALUES ("6296074483","8133950202", "2597807 10", "Carol",
   "Williams", "1984-03-01", "(514)481-2566", "6767 ch de la Côte-Saint-Luc",
-  "Côte Saint-Luc", "QC", TRUE, "carWill@gmail.com");
+  "Côte Saint-Luc", 5, TRUE, "carWill@gmail.com");
 INSERT Employee VALUES ("1988238722","5594746088", "2967560 35", "Gary",
-  "Smith", "1967-06-12", "(514)484-4049", "3472 Av Westmore", "Montreal", "QC",
+  "Smith", "1967-06-12", "(514)484-4049", "3472 Av Westmore", "Montreal", 5,
   TRUE, "garySmi@gmail.com");
 INSERT Employee VALUES ("9654156685","3050347011", "7495078 77", "Ted",
   "Johnson","1998-09-23", "(514)485-1864", "621 Côte Murray", "Westmount",
-  "QC", TRUE, "tedJohn@gmail.com");
+  5, TRUE, "tedJohn@gmail.com");
 INSERT Employee VALUES ("0426670356","6901680262", "1457287 34", "Ronald",
   "Smith","1995-06-14", "(514)642-6526", "16226 Rue Bureau",
-  "Pointe-Aux-Trembles", "QC", TRUE, "ronSmi@gmail.com");
+  "Pointe-Aux-Trembles", 5, TRUE, "ronSmi@gmail.com");
 INSERT Employee VALUES ("2589272564","2826175309", "0568018 19", "Adam",
-  "Smith", "1995-08-12", "(514)483-4346", "4840 Bonavista", "Montreal", "QC",
+  "Smith", "1995-08-12", "(514)483-4346", "4840 Bonavista", "Montreal", 5,
   TRUE, "adaSmi@gmail.com");
 INSERT Employee VALUES ("4278243142","0883386538", "5489390 84", "Wayne",
-  "Johnson","1984-07-16", "(514)486-4899", "999 Old Orchard", "Montreal", "QC",
+  "Johnson","1984-07-16", "(514)486-4899", "999 Old Orchard", "Montreal", 5,
   TRUE, "wayJon@gmail.com");
 INSERT Employee VALUES ("2221453161","3909862653", "5433470 85", "Sylvain",
-  "Williams","1978-11-14", "(514)767-3102", "1477 Rue Fayolle", "Verdun", "QC",
+  "Williams","1978-11-14", "(514)767-3102", "1477 Rue Fayolle", "Verdun", 5,
   TRUE, "sylWill@gmail.com");
 INSERT Employee VALUES ("7034521288","5025223450", "6335938 64", "Michel",
-  "Johnson","1996-09-13", "(514)597-0058", "850 Av Lachine", "Montreal", "QC",
+  "Johnson","1996-09-13", "(514)597-0058", "850 Av Lachine", "Montreal", 5,
   TRUE, "micJohn@gmail.com");
 
 INSERT Vaccination VALUES ("5418600012", 1, "2021-01-16", "6296074483", "AstraZeneca", "Olympic Stadium", "4545 Avenue Pierre-De Coubertin"); 
@@ -243,33 +243,33 @@ INSERT VaccineTransfer VALUES ("Olympic Stadium", "Hôpital de Sainte-Anne",
   "Pfizer", "2021-01-20", 100);
 
 
-INSERT PostalCode VALUES ("6860 Fielding", "Montreal", "QC", "H4V1P2");
-INSERT PostalCode VALUES ("902 Tittley", "Montreal","QC","H8R3X3");
-INSERT PostalCode VALUES ("6818 Lamont", "Montreal","QC","H7L4X8");
-INSERT PostalCode VALUES ("1720 Bourbonniere", "Montreal","QC","H1W3N1");
-INSERT PostalCode VALUES ("5962 Jogues", "Montreal","QC","J8Y4E3");
-INSERT PostalCode VALUES ("927 Rand", "Gatineau","QC","K1V6X4");
-INSERT PostalCode VALUES ("60 Du Blizzard", "Gatineau","QC","J9A0C8");
-INSERT PostalCode VALUES ("1175 De L'Esplanade", "Sherbrooke","QC","J1H1S9");
-INSERT PostalCode VALUES ("1812 Dunant", "Sherbrooke","QC","J1H1Y9");
-INSERT PostalCode VALUES ("3937 Soucy", "Jonquière","QC","G7X8T1");
-INSERT PostalCode VALUES ("4884 Boulevard Cremazie","Quebec", "QC","H2M0B0");
-INSERT PostalCode VALUES ("3619 avenue de Port-Royal","Bonaventure", "QC","J9J1C8");
-INSERT PostalCode VALUES ("6767 ch de la Côte-Saint-Luc","Côte Saint-Luc", "QC","H4V2Z6");
-INSERT PostalCode VALUES ("3472 Av Westmore", "Montreal","QC","H4V4Z6");
-INSERT PostalCode VALUES ("621 Côte Murray","Westmount","QC","H4V2Y6");
-INSERT PostalCode VALUES ("16226 Rue Bureau","Pointe-Aux-Trembles", "QC","H1A1Z1");
-INSERT PostalCode VALUES ("4840 Bonavista", "Montreal", "QC","G0C1E0");
-INSERT PostalCode VALUES ("999 Old Orchard", "Montreal", "QC","G0C1E8");
-INSERT PostalCode VALUES ("1477 Rue Fayolle", "Verdun", "QC","G3F1E0");
-INSERT PostalCode VALUES ("850 Av Lachine", "Montreal", "QC","G6C1Y0");
-INSERT PostalCode VALUES ("4545 Avenue Pierre-De Coubertin", "Montreal", "QC","G7Q2V0");
-INSERT PostalCode VALUES ("3755 Chemin de la Côte-Sainte-Catherine", "Montreal", "QC","G0S5R0");
-INSERT PostalCode VALUES ("909 Boulevard la Vérendrye O", "Gatineau", "QC","G5S2V0");
-INSERT PostalCode VALUES ("300 Rue King E", "Sherbrooke", "QC","G0S2V4");
-INSERT PostalCode VALUES ("2180, rue Fleury Est","Montreal", "QC","G0S2V0");
-INSERT PostalCode VALUES ("5425, Avenue Bessborough","Montreal", "QC","M4G3H9");
-INSERT PostalCode VALUES ("7070, boulevard Perras", "Montreal", "QC","H1E1A4");
-INSERT PostalCode VALUES ("8585, Terrasse Champlain","LaSalle", "QC","H3N2L1");
-INSERT PostalCode VALUES ("4000, boul. Lasalle","Verdun", "QC","H4G1J8");
-INSERT PostalCode VALUES ("305, boulevard des Anciens-Combattants", "Sainte-Anne-de-Bellevue", "QC","H9X1Y9");
+INSERT PostalCode VALUES ("6860 Fielding", "Montreal", 5, "H4V1P2");
+INSERT PostalCode VALUES ("902 Tittley", "Montreal",5,"H8R3X3");
+INSERT PostalCode VALUES ("6818 Lamont", "Montreal",5,"H7L4X8");
+INSERT PostalCode VALUES ("1720 Bourbonniere", "Montreal",5,"H1W3N1");
+INSERT PostalCode VALUES ("5962 Jogues", "Montreal",5,"J8Y4E3");
+INSERT PostalCode VALUES ("927 Rand", "Gatineau",5,"K1V6X4");
+INSERT PostalCode VALUES ("60 Du Blizzard", "Gatineau",5,"J9A0C8");
+INSERT PostalCode VALUES ("1175 De L'Esplanade", "Sherbrooke",5,"J1H1S9");
+INSERT PostalCode VALUES ("1812 Dunant", "Sherbrooke",5,"J1H1Y9");
+INSERT PostalCode VALUES ("3937 Soucy", "Jonquière",5,"G7X8T1");
+INSERT PostalCode VALUES ("4884 Boulevard Cremazie","Quebec", 5,"H2M0B0");
+INSERT PostalCode VALUES ("3619 avenue de Port-Royal","Bonaventure", 5,"J9J1C8");
+INSERT PostalCode VALUES ("6767 ch de la Côte-Saint-Luc","Côte Saint-Luc", 5,"H4V2Z6");
+INSERT PostalCode VALUES ("3472 Av Westmore", "Montreal",5,"H4V4Z6");
+INSERT PostalCode VALUES ("621 Côte Murray","Westmount",5,"H4V2Y6");
+INSERT PostalCode VALUES ("16226 Rue Bureau","Pointe-Aux-Trembles", 5,"H1A1Z1");
+INSERT PostalCode VALUES ("4840 Bonavista", "Montreal", 5,"G0C1E0");
+INSERT PostalCode VALUES ("999 Old Orchard", "Montreal", 5,"G0C1E8");
+INSERT PostalCode VALUES ("1477 Rue Fayolle", "Verdun", 5,"G3F1E0");
+INSERT PostalCode VALUES ("850 Av Lachine", "Montreal", 5,"G6C1Y0");
+INSERT PostalCode VALUES ("4545 Avenue Pierre-De Coubertin", "Montreal", 5,"G7Q2V0");
+INSERT PostalCode VALUES ("3755 Chemin de la Côte-Sainte-Catherine", "Montreal", 5,"G0S5R0");
+INSERT PostalCode VALUES ("909 Boulevard la Vérendrye O", "Gatineau", 5,"G5S2V0");
+INSERT PostalCode VALUES ("300 Rue King E", "Sherbrooke", 5,"G0S2V4");
+INSERT PostalCode VALUES ("2180, rue Fleury Est","Montreal", 5,"G0S2V0");
+INSERT PostalCode VALUES ("5425, Avenue Bessborough","Montreal", 5,"M4G3H9");
+INSERT PostalCode VALUES ("7070, boulevard Perras", "Montreal", 5,"H1E1A4");
+INSERT PostalCode VALUES ("8585, Terrasse Champlain","LaSalle", 5,"H3N2L1");
+INSERT PostalCode VALUES ("4000, boul. Lasalle","Verdun", 5,"H4G1J8");
+INSERT PostalCode VALUES ("305, boulevard des Anciens-Combattants", "Sainte-Anne-de-Bellevue", 5,"H9X1Y9");
